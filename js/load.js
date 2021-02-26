4313c8f2cba598ffb6ecc3d07b9ea227cc67ec43
RogerAviles22 <rogiavil@espol.edu.ec>
2021-02-26 16:53:51 -0500

    Carga JSON de imágenes-projects a HTML

--- js/load.js
+++ js/load.js
@@ -1,7 +1,20 @@
+"use strict"
+
 function loadFile() {
-    console.log("Correcto Roger");
-    console.log(fetch("portafolio.json"));
-    
+    const xhttp = new XMLHttpRequest();
+    xhttp.open("GET", "portafolio.json", true);
+    xhttp.send();
+    xhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+        let datos = JSON.parse(this.responseText);
+        let portafolio = document.getElementById("portafolio");
+        portafolio.innerHTML = '';
+
+        for(let item of datos){
+            portafolio.innerHTML += `<img class="col-lg-4 col-md-4 col-sm-6" src="images/projects/${item.imagen}" alt="${item.imagen}">`
+        }
+      }
+    };
 }
 
 loadFile();
